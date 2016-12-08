d4fafa06e6368551b1891e9134f7363d3b7092af
Maxim Ermolaev <makcoh4nk@mail.ru>
2016-12-08 23:22:47 +0300

    version 1.0

---
+++ include/Header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct sCrd
+{
+	int string;
+	int column;
+};
+
+int Shortest_way(int** martix, int str, int col);
